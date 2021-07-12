c59b5fd613764ae37449d5f435eeb3c630b0513b
Luan Uchoa <luan_uchoa@outlook.com>
2021-07-12 16:36:01 -0400

    implement methods for deleting cards

--- src/App.js
+++ src/App.js
@@ -22,13 +22,21 @@ class App extends Component{
     this.setState(novoEstado);
   }
 
+  deletarNota(index){
+    let arrayNotas = this.state.notas;
+    arrayNotas.splice(index, 1);
+    this.setState({notas:arrayNotas});
+  }
+
   render(){
     // JSX returns must have only one parent tag
     return (
       <section className="conteudo">
         <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
   
-        <ListaDeNotas notas={this.state.notas}/>
+        <ListaDeNotas
+        apagarNota={this.deletarNota.bind(this)}
+        notas={this.state.notas}/>
   
       </section>
   
