f86f9a444448a4ebb3d7d3506611766c8f8e7824
Luan Uchoa <luan_uchoa@outlook.com>
2021-07-13 16:11:56 -0400

    applying same css as showed on video class

--- src/components/ListaDeCategorias/ListaDeCategorias.jsx
+++ src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -1,17 +1,19 @@
 import React, { Component } from "react";
+import "./estilo.css";
 
 class ListaDeCategorias extends Component {
     state = { }
     render(){
-        return(<section>
-            <ul>
-                <li>Categorias</li>
-                <li>Categorias 2</li>
-                <li>Categorias 3</li>
-                <li>Categorias 4</li>
-            </ul>
-            <input type="text" />
-        </section>
+        return(
+            <section className="lista-categorias">
+                <ul className="lista-categorias_lista">
+                    <li className="lista-categorias_item">Categorias</li>
+                    <li className="lista-categorias_item">Categorias 2</li>
+                    <li className="lista-categorias_item">Categorias 3</li>
+                    <li className="lista-categorias_item">Categorias 4</li>
+                </ul>
+                <input type="text" className="lista-categorias_input" />
+            </section>
         );
     }
 }
