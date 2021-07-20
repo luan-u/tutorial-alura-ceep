30f6f5b3f00849cb8f42bd8a4710095ee5966bfe
Luan Uchoa <luan_uchoa@outlook.com>
2021-07-20 09:55:22 -0400

    applied category field in cards

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -6,6 +6,12 @@ class FormularioCadastro extends Component {
         super(props);
         this.titulo = "";
         this.texto = "";
+        this.categoria = "Sem categoria";
+    }
+
+    _handleMudancaCategoria(evento){
+        evento.stopPropagation();
+        this.categoria = evento.target.value;
     }
 
     _handleMudancaTitulo(evento){
@@ -21,7 +27,7 @@ class FormularioCadastro extends Component {
     _criarNota(evento){
         evento.preventDefault();
         evento.stopPropagation();
-        this.props.criarNota(this.titulo, this.texto);
+        this.props.criarNota(this.titulo, this.texto, this.categoria);
     }
 
     render(){
@@ -29,7 +35,8 @@ class FormularioCadastro extends Component {
             <form className="form-cadastro"
                 onSubmit={this._criarNota.bind(this)}
                 >
-                <select className="form-cadastro_input">
+                <select onChange={this._handleMudancaCategoria.bind(this)} className="form-cadastro_input">
+                    <option>Sem categoria</option>
                     {this.props.categorias.map(categoria => {
                         return <option>{categoria}</option>
                     })}  
