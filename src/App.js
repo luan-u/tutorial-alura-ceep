ef3615f7e86554eac90cb6da583e0c713619780d
Luan Uchoa <luan_uchoa@outlook.com>
2021-07-21 13:31:18 -0400

    adjustment on tab size

--- src/App.js
+++ src/App.js
@@ -9,56 +9,56 @@ import './assets/index.css';
 
 class App extends Component{
 
-  constructor(){
-    super();
-    this.state = {
-      notas: [],
-      categorias: []
+    constructor(){
+        super();
+        this.state = {
+            notas: [],
+            categorias: []
+        }
     }
-  }
 
-  adicionarCategoria(nomeCategoria){
-    const novoArrayCategorias = [...this.state.categorias, nomeCategoria];
-    const novoEstado = {...this.state, categorias:novoArrayCategorias};
-    this.setState(novoEstado);
-  }
-
-  criarNota(titulo, texto, categoria){
-    const novaNota = {titulo, texto, categoria};
-    const novoArrayDeNotas = [...this.state.notas, novaNota]
-    const novoEstado = {
-      notas:novoArrayDeNotas
+    adicionarCategoria(nomeCategoria){
+        const novoArrayCategorias = [...this.state.categorias, nomeCategoria];
+        const novoEstado = {...this.state, categorias:novoArrayCategorias};
+        this.setState(novoEstado);
     }
-    this.setState(novoEstado);
-  }
 
-  deletarNota(index){
-    let arrayNotas = this.state.notas;
-    arrayNotas.splice(index, 1);
-    this.setState({notas:arrayNotas});
-  }
+    criarNota(titulo, texto, categoria){
+        const novaNota = {titulo, texto, categoria};
+        const novoArrayDeNotas = [...this.state.notas, novaNota]
+        const novoEstado = {
+        notas:novoArrayDeNotas
+        }
+        this.setState(novoEstado);
+    }
 
-  render(){
-    // JSX returns must have only one parent tag
-    return (
-      <section className="conteudo">
-        <FormularioCadastro
-          categorias={this.state.categorias}
-          criarNota={this.criarNota.bind(this)}/>
-        <main className="conteudo-principal">
-          <ListaDeCategorias
-            adicionarCategoria={this.adicionarCategoria.bind(this)}
-            categorias={this.state.categorias} />
-          
-          <ListaDeNotas
-          apagarNota={this.deletarNota.bind(this)}
-          notas={this.state.notas}/>
-        </main>
-  
-      </section>
-  
-    );
-  }
+    deletarNota(index){
+        let arrayNotas = this.state.notas;
+        arrayNotas.splice(index, 1);
+        this.setState({notas:arrayNotas});
+    }
+
+    render(){
+        // JSX returns must have only one parent tag
+        return (
+        <section className="conteudo">
+            <FormularioCadastro
+            categorias={this.state.categorias}
+            criarNota={this.criarNota.bind(this)}/>
+            <main className="conteudo-principal">
+            <ListaDeCategorias
+                adicionarCategoria={this.adicionarCategoria.bind(this)}
+                categorias={this.state.categorias} />
+            
+            <ListaDeNotas
+            apagarNota={this.deletarNota.bind(this)}
+            notas={this.state.notas}/>
+            </main>
+    
+        </section>
+    
+        );
+    }
 }
 
 export default App;
