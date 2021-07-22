ec4e98f2673eff46c907770ff1a900c011123cfe
Luan Uchoa <luan_uchoa@outlook.com>
2021-07-22 14:33:20 -0400

    applying the new change methods

--- src/components/ListaDeCategorias/ListaDeCategorias.jsx
+++ src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 import "./estilo.css";
 
 class ListaDeCategorias extends Component {
-    state = { }
+    componentDidMount(){
+        this.props.categorias.inscrever(this._novasCategorias.bind(this));
+    }
+
+    _novasCategorias(categorias){}
 
     _handleEventoInput(e){
         if(e.key === "Enter"){
@@ -15,7 +19,7 @@ class ListaDeCategorias extends Component {
         return(
             <section className="lista-categorias">
                 <ul className="lista-categorias_lista">
-                    {this.props.categorias.map((categoria, index) => {
+                    {this.props.categorias.categorias.map((categoria, index) => {
                         return <li key={index} className="lista-categorias_item">{categoria}</li>
                     })}                  
                 </ul>
